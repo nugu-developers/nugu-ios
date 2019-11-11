8a421a18ed877851ed93ebe2ea3703edb0a284e9
Yonghoon Kwon <yhoonyk@gmail.com>
2019-11-11 16:04:34 +0900

    Restructure project (#46) (#47)
    * Restructure project (#46)
    * Create nugu-ios.xcodeproj
    * Update version in podspec
    * Adopt swift-lint except "Todo Violation"
    * Remove jazzy configuration
    * Update app group
    * Remove unnecessary file
    * Update entitlements
    * Renew and rename app-group
    * Update license in file
    * Update project setting by carthage
    * Update podspec
    * Add Cartfile
    * Update bundle identifier
    * Remove embed framework

---
+++ SampleApp/Sources/UI/Views/DisplayViews/DisplayAudioPlayerView.swift
@@ -0,0 +1,173 @@
+//
+//  DisplayAudioPlayerView.swift
+//  SampleApp-iOS
+//
+//  Created by yonghoonKwon on 20/07/2019.
+//  Copyright (c) 2019 SK Telecom Co., Ltd. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import UIKit
+
+import NuguInterface
+
+final class DisplayAudioPlayerView: UIView {
+    
+    // MARK: Properties
+    
+    @IBOutlet private weak var titleContainerView: UIView!
+    @IBOutlet private weak var serviceIconImageView: UIImageView!
+    @IBOutlet private weak var serviceLabel: UILabel!
+    
+    @IBOutlet private weak var backgroundImageView: UIImageView!
+    
+    @IBOutlet private weak var albumImageContainerView: UIView!
+    @IBOutlet private weak var albumImageView: UIImageView!
+    
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var subtitle1Label: UILabel!
+    @IBOutlet private weak var subtitle2Label: UILabel!
+    
+    @IBOutlet private weak var prevButton: UIButton!
+    @IBOutlet private weak var playPauseButton: UIButton!
+    @IBOutlet private weak var nextButton: UIButton!
+    
+    @IBOutlet private weak var progressView: UIProgressView!
+    
+    @IBOutlet private weak var elapsedTimeLabel: UILabel!
+    @IBOutlet private weak var durationTimeLabel: UILabel!
+    
+    private var audioProgressTimer: DispatchSourceTimer?
+    private let audioProgressTimerQueue = DispatchQueue(label: "com.sktelecom.romaine.DisplayAudioPlayerView.audioProgress")
+    
+    var onCloseButtonClick: (() -> Void)?
+    
+    var displayItem: AudioPlayerDisplayTemplate.AudioPlayer? {
+        didSet {
+            let template = displayItem?.template
+            serviceLabel.text = template?.title.text
+            serviceIconImageView.loadImage(from: template?.title.iconUrl)
+            
+            if let imageUrl = template?.content.imageUrl {
+                albumImageView.loadImage(from: imageUrl)
+                albumImageContainerView.isHidden = false
+            } else {
+                albumImageContainerView.isHidden = true
+            }
+            
+            titleLabel.text = template?.content.title
+            subtitle1Label.text = template?.content.subtitle ?? template?.content.subtitle1
+            subtitle2Label.text = template?.content.subtitle2
+            backgroundImageView.loadImage(from: template?.content.backgroundImageUrl)
+            
+            startProgressTimer()
+        }
+    }
+    
+    var audioPlayerState: AudioPlayerState? {
+        didSet {
+            playPauseButton.isSelected = (audioPlayerState == .playing)
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        loadFromXib()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        loadFromXib()
+    }
+    
+    private func loadFromXib() {
+        // swiftlint:disable force_cast
+        let view = Bundle.main.loadNibNamed("DisplayAudioPlayerView", owner: self)?.first as! UIView
+        // swiftlint:enable force_cast
+        view.frame = bounds
+        addSubview(view)
+        albumImageView.layer.cornerRadius = 4.0
+        addBorderToTitleContainerView()
+    }
+    
+    private func addBorderToTitleContainerView() {
+        titleContainerView.layer.cornerRadius = titleContainerView.bounds.size.height / 2.0
+        titleContainerView.layer.borderColor = UIColor(rgbHexValue: 0xc9cacc).cgColor
+        titleContainerView.layer.borderWidth = 1.0
+    }
+}
+
+// MARK: - IBActions
+
+private extension DisplayAudioPlayerView {
+    @IBAction private func previousButtonDidClick(_ button: UIButton) {
+        NuguCentralManager.shared.client.audioPlayerAgent?.prev()
+    }
+    
+    @IBAction private func playPauseDidClick(_ button: UIButton) {
+        if button.isSelected {
+            NuguCentralManager.shared.client.audioPlayerAgent?.pause()
+        } else {
+            NuguCentralManager.shared.client.audioPlayerAgent?.play()
+        }
+    }
+    
+    @IBAction private func nextButtonDidClick(_ button: UIButton) {
+        NuguCentralManager.shared.client.audioPlayerAgent?.next()
+    }
+    
+    @IBAction private func closeButtonDidClick(_ button: UIButton) {
+        onCloseButtonClick?()
+    }
+}
+
+// MARK: - Progress Setting
+
+private extension DisplayAudioPlayerView {
+    func setAudioPlayerProgress() {
+        DispatchQueue.main.async { [weak self] in
+            guard let elapsedTimeAsInt = NuguCentralManager.shared.client.audioPlayerAgent?.offset,
+                let durationAsInt = NuguCentralManager.shared.client.audioPlayerAgent?.duration else {
+                    self?.elapsedTimeLabel.text = nil
+                    self?.durationTimeLabel.text = nil
+                    self?.progressView.isHidden = true
+                    return
+            }
+            let elapsedTime = Float(elapsedTimeAsInt)
+            let duration = Float(durationAsInt)
+            self?.elapsedTimeLabel.text = Int(elapsedTime).secondTimeString
+            self?.durationTimeLabel.text = Int(duration).secondTimeString
+            UIView.animate(withDuration: 1.0, animations: { [weak self] in
+                self?.progressView.setProgress(elapsedTime/duration, animated: true)
+            })
+        }
+    }
+    
+    private func startProgressTimer() {
+        audioProgressTimer?.cancel()
+        audioProgressTimer = DispatchSource.makeTimerSource(queue: audioProgressTimerQueue)
+        audioProgressTimer?.schedule(deadline: .now(), repeating: 1.0)
+        audioProgressTimer?.setEventHandler(handler: { [weak self] in
+            self?.setAudioPlayerProgress()
+        })
+        audioProgressTimer?.resume()
+    }
+    
+    private func stopProgressTimer() {
+        audioProgressTimer?.cancel()
+        audioProgressTimer = nil
+    }
+}
---
+++ SampleApp/Sources/UI/Views/NuguToastManager.swift
@@ -0,0 +1,119 @@
+//
+//  NuguToastManager.swift
+//  SampleApp-iOS
+//
+//  Created by jin kim on 00/09/2019.
+//  Copyright (c) 2019 SK Telecom Co., Ltd. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import UIKit
+
+final class NuguToastManager {
+    private struct ToastConst {
+        static let cornerRadius = CGFloat(4)
+        static let textVerticalPadding = 24.0
+        static let textHorizontalPadding = 14.0
+        static let viewMargin = 8.0
+        static let bottomMargin = 8.0
+        static let animationDuration = 0.3
+        static let showingDuration = 7.0
+        static let textColor = UIColor.white
+        static let textFont = UIFont.systemFont(ofSize: 14.0)
+        static let backgroundColor = UIColor(rgbHexValue: 0x323232)
+    }
+    
+    static let shared = NuguToastManager()
+    
+    private lazy var toastView = UIView()
+    private lazy var toastLabel = UILabel()
+    
+    private var hideAnimationWorkItem: DispatchWorkItem?
+}
+
+extension NuguToastManager {
+    func showToast(message: String?) {
+        guard let window = UIApplication.shared.keyWindow else { return }
+        guard let toastMessage = message, toastMessage.count > 0 else { return }
+        
+        hideAnimationWorkItem?.cancel()
+        hideAnimationWorkItem = nil
+        
+        toastLabel.textAlignment = .center
+        toastLabel.textColor = ToastConst.textColor
+        toastLabel.font = ToastConst.textFont
+        toastLabel.numberOfLines = 0
+        toastLabel.preferredMaxLayoutWidth = window.bounds.size.width - CGFloat((ToastConst.viewMargin + ToastConst.textHorizontalPadding) * 2)
+        toastLabel.text = toastMessage
+        toastLabel.frame = CGRect(origin: CGPoint(x: ToastConst.textHorizontalPadding,
+                                                  y: ToastConst.textVerticalPadding),
+                                  size: toastLabel.intrinsicContentSize)
+        
+        toastView.addSubview(toastLabel)
+        toastView.backgroundColor = ToastConst.backgroundColor
+        toastView.layer.cornerRadius = ToastConst.cornerRadius
+        toastView.frame = CGRect(x: 0, y: 0,
+                                 width: window.bounds.size.width - CGFloat(ToastConst.viewMargin * 2),
+                                 height: toastLabel.intrinsicContentSize.height + CGFloat(ToastConst.textVerticalPadding * 2))
+        toastView.center = CGPoint(x: window.center.x,
+                                   y: window.bounds.size.height - (toastView.bounds.size.height/2) - CGFloat(ToastConst.bottomMargin) - SampleApp.bottomSafeAreaHeight)
+        toastLabel.center = CGPoint(x: toastView.frame.size.width/2,
+                                    y: toastView.frame.size.height/2)
+        toastView.alpha = 0
+        
+        hideAnimationWorkItem = DispatchWorkItem {
+            UIView.animate(
+                withDuration: ToastConst.animationDuration,
+                animations: { [weak self] in
+                    self?.toastView.alpha = 0
+                },
+                completion: { [weak self] _ in
+                    self?.toastLabel.removeFromSuperview()
+                    self?.toastView.removeFromSuperview()
+            })
+        }
+        
+        UIView.animate(
+            withDuration: ToastConst.animationDuration,
+            animations: { [weak self] in
+                guard let toastView = self?.toastView else { return }
+                toastView.alpha = 1
+                window.addSubview(toastView)
+                window.bringSubviewToFront(toastView)
+            },
+            completion: { [weak self] _ in
+                if let hideAnimationWorkItem = self?.hideAnimationWorkItem {
+                    DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + ToastConst.showingDuration,
+                                                  execute: hideAnimationWorkItem)
+                }
+        })
+    }
+    
+    func hideToastIfNeeded() {
+        if let hideAnimationWorkItem = self.hideAnimationWorkItem {
+            hideAnimationWorkItem.cancel()
+            self.hideAnimationWorkItem = nil
+            UIView.animate(
+                withDuration: ToastConst.animationDuration,
+                animations: {
+                    [weak self] in
+                    self?.toastView.alpha = 0
+                },
+                completion: { [weak self] _ in
+                    self?.toastLabel.removeFromSuperview()
+                    self?.toastView.removeFromSuperview()
+            })
+        }
+    }
+}
