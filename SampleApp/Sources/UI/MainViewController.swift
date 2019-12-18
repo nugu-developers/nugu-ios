ff0dbe457065b81f81c2b388b8cd8c7bf653c743
jin-marino <nuclearcom1@hanmail.net>
2019-12-18 09:19:43 +0900

    Add `DisplayWeatherListView` and `DisplayWeatherListTemplate` (#149) (#165)
    * Upgrade `DisplayAgent` s version to 1.1
    
    * Add DisplayWeatherView
    
    * Remove default ui values of `DisplayView`s
    
    * Rename `DisplayWeatherView`'s ui components
    
    * Add comments
    
    * Upgrade `DisplayWeatherView` to handle `Display.Weather2` model also
    
    * Add `DisplayWeatherListView` and `DisplayWeatherListTemplate to handle `Display.Weather3`, `Display.Weather4` models
    
    * Remove unnecessary white spaces

--- SampleApp/Sources/UI/MainViewController.swift
+++ SampleApp/Sources/UI/MainViewController.swift
@@ -308,8 +308,10 @@ private extension MainViewController {
             displayView = DisplayListView(frame: view.frame)
         case "Display.TextList3", "Display.TextList4":
             displayView = DisplayBodyListView(frame: view.frame)
-        case "Display.Weather1":
+        case "Display.Weather1", "Display.Weather2":
             displayView = DisplayWeatherView(frame: view.frame)
+        case "Display.Weather3", "Display.Weather4":
+            displayView = DisplayWeatherListView(frame: view.frame)
         default:
             // Draw your own DisplayView with DisplayTemplate.payload and set as self.displayView
             break
