06196700821af98771b08e2ff3e50370225c5322
jin-marino <nuclearcom1@hanmail.net>
2020-02-13 11:46:00 +0900

    Add comments to `DisplayAgent` related methods

--- NuguAgents/Interface/Display/DisplayAgentDelegate.swift
+++ NuguAgents/Interface/Display/DisplayAgentDelegate.swift
@@ -33,13 +33,13 @@ public protocol DisplayAgentDelegate: class {
     /// - Parameter reason: Reason to clear the template.
     func displayAgentShouldClear(template: DisplayTemplate, reason: DisplayTemplate.ClearReason)
     
-    /// Tells the delegate that the specified template should move focus with given direction.
-    /// Should return whether successed or not.
+    /// Tells the delegate that the displayed template should move focus with given direction.
+    /// Should return whether succeeded or not.
     /// - Parameter direction: Direction to move focus.
     func displayAgentShouldMoveFocus(direction: DisplayControlPayload.Direction) -> Bool
     
-    /// Tells the delegate that the specified template should scroll with given direction.
-    /// Should return whether successed or not.
+    /// Tells the delegate that the displayed template should scroll with given direction.
+    /// Should return whether succeeded or not.
     /// - Parameter reason: Direction to scroll.
     func displayAgentShouldScroll(direction: DisplayControlPayload.Direction) -> Bool
     
