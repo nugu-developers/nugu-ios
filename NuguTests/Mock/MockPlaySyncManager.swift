20c5e8b379f78818115cfb78e827d1565fd2f506
Yonghoon Kwon <yhoonyk@gmail.com>
2020-03-09 15:16:15 +0900

    Add test-case for capability-agents (#241)

---
+++ NuguTests/Mock/MockPlaySyncManager.swift
@@ -0,0 +1,51 @@
+//
+//  MockPlaySyncManager.swift
+//  NuguTests
+//
+//  Created by yonghoonKwon on 2020/03/02.
+//  Copyright (c) 2020 SK Telecom Co., Ltd. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import Foundation
+
+import NuguCore
+
+class MockPlaySyncManager: PlaySyncManageable {
+    var playServiceIds: [String] = []
+    
+    func prepareSync(delegate: PlaySyncDelegate, dialogRequestId: String, playServiceId: String?) {
+        //
+    }
+    
+    func startSync(delegate: PlaySyncDelegate, dialogRequestId: String, playServiceId: String?) {
+        //
+    }
+    
+    func cancelSync(delegate: PlaySyncDelegate, dialogRequestId: String, playServiceId: String?) {
+        //
+    }
+    
+    func releaseSync(delegate: PlaySyncDelegate, dialogRequestId: String, playServiceId: String?) {
+        //
+    }
+    
+    func releaseSyncImmediately(dialogRequestId: String, playServiceId: String?) {
+        //
+    }
+    
+    func contextInfoRequestContext(completionHandler: @escaping (ContextInfo?) -> Void) {
+        completionHandler(nil)
+    }
+}
