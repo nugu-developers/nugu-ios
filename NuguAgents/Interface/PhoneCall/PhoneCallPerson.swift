c5edf280a4fa7944e0762309fdd6de55604dba89
yHoonyK <yhoonyk@gmail.com>
2020-05-20 14:28:33 +0900

    Add public initializer

--- NuguAgents/Interface/PhoneCall/PhoneCallPerson.swift
+++ NuguAgents/Interface/PhoneCall/PhoneCallPerson.swift
@@ -25,11 +25,21 @@ public struct PhoneCallPerson: Codable {
         public struct BusinessHours: Codable {
             public let open: String?
             public let close: String?
+            
+            init(open: String?, close: String?) {
+                self.open = open
+                self.close = close
+            }
         }
         
         public struct History: Codable {
             public let time: String
             public let type: String
+            
+            init(time: String, type: String) {
+                self.time = time
+                self.type = type
+            }
         }
         
         public let type: String
@@ -41,10 +51,39 @@ public struct PhoneCallPerson: Codable {
         public let businessHours: BusinessHours?
         public let history: History?
         public let numInCallHistory: String
+        
+        init(
+            type: String,
+            label: String?,
+            profileImgUrl: String?,
+            category: String?,
+            address: String?,
+            number: String?,
+            businessHours: BusinessHours?,
+            history: History?,
+            numInCallHistory: String
+        ) {
+            self.type = type
+            self.label = label
+            self.profileImgUrl = profileImgUrl
+            self.category = category
+            self.address = address
+            self.number = number
+            self.businessHours = businessHours
+            self.history = history
+            self.numInCallHistory = numInCallHistory
+        }
     }
     
     public let name: String
     public let token: String
     public let score: String?
     public let contacts: [Contact]?
+    
+    init(name: String, token: String, score: String?, contacts: [Contact]?) {
+        self.name = name
+        self.token = token
+        self.score = score
+        self.contacts = contacts
+    }
 }
