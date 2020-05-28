68824b373190c040c9348f13b9e1b5372b4650d3
Minddol <minddol@gmail.com>
2020-05-28 13:11:46 +0900

    Remove `System.SynchronizedState` event that is called when initializing `NuguClient`. (#332)

--- NuguClientKit/Sources/Client/NuguClient.swift
+++ NuguClientKit/Sources/Client/NuguClient.swift
@@ -138,8 +138,6 @@ public class NuguClient {
         setupAudioSessionRequester()
         setupDialogStateAggregator()
         setupStreamDataRouter()
-        
-        systemAgent.sendSynchronizeStateEvent()
     }
 }
     
