a91a1abe109bbd556be588b303936d1176104e7a
Minddol <minddol@gmail.com>
2020-08-07 17:56:58 +0900

    Fixes a build error.

--- SampleApp/Sources/Manager/NuguCentralManager.swift
+++ SampleApp/Sources/Manager/NuguCentralManager.swift
@@ -55,7 +55,7 @@ final class NuguCentralManager {
         }
     }()
     
-    var isTextAgentInProcess = false
+    private var startMicWorkItem: DispatchWorkItem?
 
     // Audio input source
     private let micQueue = DispatchQueue(label: "central_manager_mic_input_queue")
