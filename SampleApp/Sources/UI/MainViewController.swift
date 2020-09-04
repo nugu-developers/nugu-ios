9394ee32acafd9b35b47c5431c71be34dbecdd1f
Minddol <minddol@gmail.com>
2020-09-04 12:45:05 +0900

    Modify access control of `DialogStateAggregator` properties.
    * Modify access control of `DialogStateAggregator.sessionActivated` and `DialogStateAggregator.isMultiturn` from private to public.

--- SampleApp/Sources/UI/MainViewController.swift
+++ SampleApp/Sources/UI/MainViewController.swift
@@ -724,20 +724,21 @@ extension MainViewController: DisplayAgentDelegate {
     }
     
     func displayAgentShouldRender(template: DisplayTemplate, completion: @escaping (AnyObject?) -> Void) {
-        log.debug("")
+        log.debug("templateId: \(template.templateId)")
         DispatchQueue.main.async { [weak self] in
             completion(self?.addDisplayView(displayTemplate: template))
         }
     }
     
     func displayAgentShouldUpdate(templateId: String, template: DisplayTemplate) {
+        log.debug("templateId: \(templateId)")
         DispatchQueue.main.async { [weak self] in
             self?.updateDisplayView(displayTemplate: template)
         }
     }
     
     func displayAgentDidClear(templateId: String) {
-        log.debug("")
+        log.debug("templateId: \(templateId)")
         DispatchQueue.main.async { [weak self] in
             self?.dismissDisplayView()
         }
