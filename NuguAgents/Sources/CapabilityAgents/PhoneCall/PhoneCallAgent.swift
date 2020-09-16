de6354e59e8a363c595c6e13951777bcab5ea525
Yonghoon Kwon <yhoonyk@gmail.com>
2020-09-16 21:34:22 +0900

    Update `PhoneCallAgent` for 'MakeCall' directive (#512)

--- NuguAgents/Sources/CapabilityAgents/PhoneCall/PhoneCallAgent.swift
+++ NuguAgents/Sources/CapabilityAgents/PhoneCall/PhoneCallAgent.swift
@@ -34,10 +34,15 @@ public class PhoneCallAgent: PhoneCallAgentProtocol {
     private let upstreamDataSender: UpstreamDataSendable
     private let interactionControlManager: InteractionControlManageable
     
+    private let phoneCallDispatchQueue = DispatchQueue(label: "com.sktelecom.romaine.phonecall_agent", qos: .userInitiated)
+    
+    // Current directive info
+    private var currentMakeCallMessageId: String?
+    
     // Handleable Directive
     private lazy var handleableDirectiveInfos: [DirectiveHandleInfo] = [
         DirectiveHandleInfo(namespace: capabilityAgentProperty.name, name: "SendCandidates", blockingPolicy: BlockingPolicy(medium: .none, isBlocking: false), directiveHandler: handleSendCandidates),
-        DirectiveHandleInfo(namespace: capabilityAgentProperty.name, name: "MakeCall", blockingPolicy: BlockingPolicy(medium: .audio, isBlocking: false), directiveHandler: handleMakeCall)
+        DirectiveHandleInfo(namespace: capabilityAgentProperty.name, name: "MakeCall", blockingPolicy: BlockingPolicy(medium: .audio, isBlocking: false), preFetch: prefetchMakeCall, directiveHandler: handleMakeCall)
     ]
     
     public init(
@@ -121,70 +126,118 @@ extension PhoneCallAgent: ContextInfoDelegate {
 private extension PhoneCallAgent {
     func handleSendCandidates() -> HandleDirective {
         return { [weak self] directive, completion in
-            guard let payloadDictionary = directive.payloadDictionary else {
-                completion(.failed("Invalid payload"))
-                return
-            }
-            guard let payloadData = try? JSONSerialization.data(withJSONObject: payloadDictionary, options: []),
-                let candidatesItem = try? JSONDecoder().decode(PhoneCallCandidatesItem.self, from: payloadData) else {
-                    completion(.failed("Invalid candidateItem in payload"))
-                    return
-            }
-            guard let self = self, let delegate = self.delegate else {
+            
+            guard let self = self else {
                 completion(.canceled)
                 return
             }
-            defer { completion(.finished) }
             
-            // TODO: Check interaction mode in payload.
-            self.interactionControlManager.start(mode: .multiTurn, category: self.capabilityAgentProperty.category)
-            delegate.phoneCallAgentDidReceiveSendCandidates(
-                item: candidatesItem,
-                dialogRequestId: directive.header.dialogRequestId
-            )
+            self.phoneCallDispatchQueue.async { [weak self] in
+                guard let self = self, let delegate = self.delegate else {
+                    completion(.canceled)
+                    return
+                }
+                
+                guard let payloadDictionary = directive.payloadDictionary else {
+                    completion(.failed("Invalid payload"))
+                    return
+                }
+                guard let payloadData = try? JSONSerialization.data(withJSONObject: payloadDictionary, options: []),
+                    let candidatesItem = try? JSONDecoder().decode(PhoneCallCandidatesItem.self, from: payloadData) else {
+                        completion(.failed("Invalid candidateItem in payload"))
+                        return
+                }
+                
+                defer { completion(.finished) }
+                
+                // TODO: Check interaction mode in payload.
+                self.interactionControlManager.start(mode: .multiTurn, category: self.capabilityAgentProperty.category)
+                delegate.phoneCallAgentDidReceiveSendCandidates(
+                    item: candidatesItem,
+                    dialogRequestId: directive.header.dialogRequestId
+                )
+            }
+        }
+    }
+    
+    func prefetchMakeCall() -> PrefetchDirective {
+        return { [weak self] directive in
+            self?.phoneCallDispatchQueue.sync { [weak self] in
+                self?.currentMakeCallMessageId = directive.header.messageId
+            }
         }
     }
     
     func handleMakeCall() -> HandleDirective {
         return { [weak self] directive, completion in
-            guard let payloadDictionary = directive.payloadDictionary else {
-                completion(.failed("Invalid payload"))
+            guard let self = self else {
+                completion(.canceled)
                 return
             }
-            guard let playServiceId = payloadDictionary["playServiceId"] as? String,
-                let callType = payloadDictionary["callType"] as? String,
-                let phoneCallType = PhoneCallType(rawValue: callType) else {
-                    completion(.failed("Invalid callType or playServiceId in payload"))
+            
+            self.phoneCallDispatchQueue.async { [weak self] in
+                guard let self = self else {
+                    completion(.canceled)
                     return
-            }
-            guard let recipientDictionary = payloadDictionary["recipient"] as? [String: AnyHashable],
-                let recipientData = try? JSONSerialization.data(withJSONObject: recipientDictionary, options: []),
-                let recipientPerson = try? JSONDecoder().decode(PhoneCallPerson.self, from: recipientData) else {
-                    completion(.failed("Invalid recipient in payload"))
-                    return
-            }
-            defer { completion(.finished) }
-
-            if let errorCode = self?.delegate?.phoneCallAgentDidReceiveMakeCall(callType: phoneCallType, recipient: recipientPerson, dialogRequestId: directive.header.dialogRequestId) {
-                guard let self = self else { return }
+                }
                 
-                // Failed to makeCall
-                let eventIdentifier = EventIdentifier()
-                self.contextManager.getContexts(namespace: self.capabilityAgentProperty.name) { [weak self] contextPayload in
-                    guard let self = self else { return }
-                    
-                    self.upstreamDataSender.sendEvent(
-                        Event(
-                            playServiceId: playServiceId,
-                            typeInfo: .makeCallFailed(errorCode: errorCode, callType: phoneCallType)
-                        ).makeEventMessage(
-                            property: self.capabilityAgentProperty,
-                            eventIdentifier: eventIdentifier,
-                            contextPayload: contextPayload
-                        )
-                    )
+                guard self.currentMakeCallMessageId == directive.header.messageId else {
+                    completion(.canceled)
+                    log.info("Message id does not match")
+                    return
+                }
+                
+                guard let payloadDictionary = directive.payloadDictionary else {
+                    completion(.failed("Invalid payload"))
+                    return
+                }
+                
+                guard let playServiceId = payloadDictionary["playServiceId"] as? String,
+                    let callType = payloadDictionary["callType"] as? String,
+                    let phoneCallType = PhoneCallType(rawValue: callType) else {
+                        completion(.failed("Invalid callType or playServiceId in payload"))
+                        return
+                }
+                
+                guard let recipientDictionary = payloadDictionary["recipient"] as? [String: AnyHashable],
+                    let recipientData = try? JSONSerialization.data(withJSONObject: recipientDictionary, options: []),
+                    let recipientPerson = try? JSONDecoder().decode(PhoneCallPerson.self, from: recipientData) else {
+                        completion(.failed("Invalid recipient in payload"))
+                        return
+                }
+                
+                defer { completion(.finished) }
+
+                if let errorCode = self.delegate?.phoneCallAgentDidReceiveMakeCall(
+                    callType: phoneCallType,
+                    recipient: recipientPerson,
+                    dialogRequestId: directive.header.dialogRequestId
+                    ) {
+                    self.sendMakeCallFailed(playServiceId: playServiceId, errorCode: errorCode, phoneCallType: phoneCallType)
                 }
             }
+        }
+    }
+}
+
+// MARK: - Private (Event)
+
+private extension PhoneCallAgent {
+    func sendMakeCallFailed(playServiceId: String, errorCode: PhoneCallErrorCode, phoneCallType: PhoneCallType) {
+        let eventIdentifier = EventIdentifier()
+        self.contextManager.getContexts(namespace: self.capabilityAgentProperty.name) { [weak self] contextPayload in
+            guard let self = self else { return }
+            
+            self.upstreamDataSender.sendEvent(
+                Event(
+                    playServiceId: playServiceId,
+                    typeInfo: .makeCallFailed(errorCode: errorCode, callType: phoneCallType)
+                ).makeEventMessage(
+                    property: self.capabilityAgentProperty,
+                    eventIdentifier: eventIdentifier,
+                    contextPayload: contextPayload
+                )
+            )
         }
     }
 }
