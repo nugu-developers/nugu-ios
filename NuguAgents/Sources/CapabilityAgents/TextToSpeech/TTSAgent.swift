8599c33d99f910a043c08ba7adc69938dc033f44
Minddol <minddol@gmail.com>
2020-11-24 11:52:27 +0900

    Make sure that the `StreaDataDelegate` functions are called first to satisfy the statistical requirements.

--- NuguAgents/Sources/CapabilityAgents/TextToSpeech/TTSAgent.swift
+++ NuguAgents/Sources/CapabilityAgents/TextToSpeech/TTSAgent.swift
@@ -59,7 +59,7 @@ public final class TTSAgent: TTSAgentProtocol {
     private let directiveSequencer: DirectiveSequenceable
     private let upstreamDataSender: UpstreamDataSendable
     
-    private let ttsDeleageteDispatchQueue = DispatchQueue(label: "com.sktelecom.romaine.tts_agent_delegate")
+    private let ttsDelegateDispatchQueue = DispatchQueue(label: "com.sktelecom.romaine.tts_agent_delegate")
     private let ttsDispatchQueue = DispatchQueue(label: "com.sktelecom.romaine.tts_agent", qos: .userInitiated)
     
     private let delegates = DelegateSet<TTSAgentDelegate>()
@@ -101,7 +101,7 @@ public final class TTSAgent: TTSAgentProtocol {
             // Notify delegates only if the agent's status changes.
             if oldValue != ttsState {
                 let state = ttsState
-                delegates.notify(queue: ttsDeleageteDispatchQueue) { delegate in
+                delegates.notify(queue: ttsDelegateDispatchQueue) { delegate in
                     delegate.ttsAgentDidChange(state: state, header: player.header)
                 }
             }
@@ -402,7 +402,7 @@ private extension TTSAgent {
                 log.debug(directive.header.messageId)
                 self.currentPlayer = player
                 self.focusManager.requestFocus(channelDelegate: self)
-                self.delegates.notify(queue: self.ttsDeleageteDispatchQueue) { delegate in
+                self.delegates.notify(queue: self.ttsDelegateDispatchQueue) { delegate in
                     delegate.ttsAgentDidReceive(text: player.payload.text, header: player.header)
                 }
                 
