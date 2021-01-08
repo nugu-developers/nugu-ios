0365f5da2956d45afcfe7b95feb05ec05ee77a20
childc <skimdcc@gmail.com>
2021-01-08 20:02:08 +0900

    Make `ObserverKey` to remove observer in the future.

--- NuguUtils/Sources/PropertyWrappers/Observing.swift
+++ NuguUtils/Sources/PropertyWrappers/Observing.swift
@@ -23,7 +23,12 @@ import Foundation
 @propertyWrapper
 public class Observing<Value: Equatable> {
     private var value: Value
-    lazy var observerContainer: ObserverContainer = ObserverContainer(self)
+    @Atomic private var nextId = 0
+    private lazy var observerContainer: ObserverContainer = ObserverContainer(self)
+    
+    public var notificationQueue: DispatchQueue?
+    public var additionalInfo: [String: Any]?
+    public var duplicatedNotify = true
     
     public init(wrappedValue: Value) {
         value = wrappedValue
@@ -35,10 +40,9 @@ public class Observing<Value: Equatable> {
         }
 
         set {
-            let notifyable = duplicatedNotify || (value != newValue)
             value = newValue
 
-            if notifyable {
+            if duplicatedNotify || (value != newValue) {
                 observerContainer.notify(newValue)
             }
         }
@@ -47,38 +51,38 @@ public class Observing<Value: Equatable> {
     public var projectedValue: ObserverContainer {
         return observerContainer
     }
-    
-    public var notificationQueue: DispatchQueue?
-    public var additionalInfo: [String: Any]?
-    public var duplicatedNotify = true
+}
 
-    public class ObserverContainer: ObserverContainable {
+// MARK: - Container
+
+public extension Observing {
+    class ObserverContainer {
         public typealias ObservingType = (_ newValue: Value, _ addtionalInfo: [String: Any]?) -> Void
         private let base: Observing<Value>
-        private var observers: [ObservingType?] = []
+        private var observers = [ObserverKey: ObservingType]()
         
         init(_ base: Observing<Value>) {
             self.base = base
         }
 
-        @discardableResult public func addObserver(_ observer: @escaping ObservingType) -> Int {
-            let currentIdx = observers.count
-            observers.append(observer)
-
-            return currentIdx
+        @discardableResult public func addObserver(_ observer: @escaping ObservingType) -> ObserverKey {
+            let key = ObserverKey.generateKey(base: base)
+            observers[key] = observer
+            base.nextId += 1
+            
+            return key
         }
 
-        public func removeObserver(_ observerKey: Int) {
-            observers.remove(at: observerKey)
+        public func removeObserver(_ observerKey: ObserverKey) {
+            observers[observerKey] = nil
         }
 
         fileprivate func notify(_ newValue: Value) {
             let notifyWorkItem = DispatchWorkItem { [weak self] in
                 guard let self = self else { return }
 
-                self.observers = self.observers.compactMap { $0 }
-                self.observers.forEach { (observer) in
-                    observer?(newValue, self.base.additionalInfo)
+                self.observers.values.forEach { (observer) in
+                    observer(newValue, self.base.additionalInfo)
                 }
             }
 
@@ -90,10 +94,22 @@ public class Observing<Value: Equatable> {
             queue.async(execute: notifyWorkItem)
         }
     }
-}
+    
+    struct ObserverKey: Hashable {
+        private let id: Int
+        
+        private init(id: Int) {
+            self.id = id
+        }
+        
+        fileprivate static func generateKey(base: Observing) -> ObserverKey {
+            var key: ObserverKey!
+            base._nextId.mutate {
+                key = ObserverKey(id: $0)
+                $0 += 1
+            }
 
-public protocol ObserverContainable {
-    associatedtype ObservingType
-    @discardableResult func addObserver(_ observer: ObservingType) -> Int
-    func removeObserver(_ observerKey: Int)
+            return key
+        }
+    }
 }
