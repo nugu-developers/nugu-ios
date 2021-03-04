57b4810a446f2b8acba6a353bc2e643c3b26667f
childc <skimdcc@gmail.com>
2021-03-04 19:05:30 +0900

    Add `Contact` object to `MessageCandidatesItem` (#766)

--- NuguAgents/Sources/CapabilityAgents/Message/MessageCandidatesItem.swift
+++ NuguAgents/Sources/CapabilityAgents/Message/MessageCandidatesItem.swift
@@ -33,7 +33,7 @@ public struct MessageCandidatesItem {
     /// The candidate searched for play service.
     ///
     /// If nil, there are no search results.
-    public let candidates: [MessageCandidatesItem]?
+    public let candidates: [Contact]?
     /// The message to be used for outgoing.
     public let messageToSend: MessageToSendItem?
     /// <#Description#>
@@ -60,8 +60,65 @@ extension MessageCandidatesItem: Codable {
         intent = try? container.decode(String.self, forKey: .intent)
         recipientIntended = try? container.decode(MessageRecipientIntended.self, forKey: .recipientIntended)
         searchScene = try? container.decode(String.self, forKey: .searchScene)
-        candidates = try? container.decode([MessageCandidatesItem].self, forKey: .candidates)
+        candidates = try? container.decode([Contact].self, forKey: .candidates)
         messageToSend = try? container.decode(MessageToSendItem.self, forKey: .messageToSend)
         interactionControl = try? container.decode(InteractionControl.self, forKey: .interactionControl)
+    }
+}
+
+// MARK: - Contact
+
+extension MessageCandidatesItem {
+    public struct Contact: Codable {
+        public enum Label: String, Codable {
+            case mobile = "MOBILE"
+            case company = "COMPANY"
+            case home = "HOME"
+            case userDefined = "USER_DEFINED"
+        }
+        
+        public enum ContactType: String, Codable {
+            case contact = "CONTACT"
+            case exchange = "EXCHANGE"
+            case t114 = "T114"
+            case none = "NONE"
+        }
+        
+        public struct Message: Codable {
+            public enum MessageType: String, Codable {
+                case sms = "SMS"
+                case mms = "MMS"
+            }
+            
+            public let text: String
+            public let type: MessageType
+            public let time: String?
+            public let numInMessageHistory: String?
+            public let token: String?
+            public let score: String?
+            
+            public init(text: String, type: MessageType, time: String?, numInMessageHistory: String?, token: String?, score: String?) {
+                self.text = text
+                self.type = type
+                self.time = time
+                self.numInMessageHistory = numInMessageHistory
+                self.token = token
+                self.score = score
+            }
+        }
+        
+        public let name: String?
+        public let type: ContactType?
+        public let number: String?
+        public let profileImgUrl: String?
+        public let message: Message?
+        
+        public init(name: String?, type: ContactType, number: String?, profileImgUrl: String?, message: Message?) {
+            self.name = name
+            self.type = type
+            self.number = number
+            self.profileImgUrl = profileImgUrl
+            self.message = message
+        }
     }
 }
