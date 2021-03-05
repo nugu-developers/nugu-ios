e461bf13b6fa1c977a2d8f18f930d0edf1e6021d
Minddol <minddol@gmail.com>
2021-03-05 16:05:02 +0900

    Fixes a review issue.

--- NuguClientKit/Sources/Presenter/VoiceChromePresenter.swift
+++ NuguClientKit/Sources/Presenter/VoiceChromePresenter.swift
@@ -131,7 +131,7 @@ public extension VoiceChromePresenter {
         log.debug("")
         
         if let chipsData = chipsData {
-            nuguVoiceChrome.setChipsData(chipsData: chipsData) { [weak self] chips in
+            nuguVoiceChrome.setChipsData(chipsData) { [weak self] chips in
                 self?.delegate?.voiceChromeChipsDidClick(chips: chips)
             }
         }
@@ -186,12 +186,12 @@ private extension VoiceChromePresenter {
     }
     
     func setChipsButton(actionList: [(text: String, token: String?)], normalList: [(text: String, token: String?)]) {
-        var chipsButtonList = [NuguChipsButton.NuguChipsButtonType]()
+        var chipsData = [NuguChipsButton.NuguChipsButtonType]()
         let actionButtonList = actionList.map { NuguChipsButton.NuguChipsButtonType.action(text: $0.text, token: $0.token) }
-        chipsButtonList.append(contentsOf: actionButtonList)
+        chipsData.append(contentsOf: actionButtonList)
         let normalButtonList = normalList.map { NuguChipsButton.NuguChipsButtonType.normal(text: $0.text, token: $0.token) }
-        chipsButtonList.append(contentsOf: normalButtonList)
-        nuguVoiceChrome.setChipsData(chipsData: chipsButtonList) { [weak self] chips in
+        chipsData.append(contentsOf: normalButtonList)
+        nuguVoiceChrome.setChipsData(chipsData) { [weak self] chips in
             self?.delegate?.voiceChromeChipsDidClick(chips: chips)
         }
     }
