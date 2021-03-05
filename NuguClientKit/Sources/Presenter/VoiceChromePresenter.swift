72fda2a0eaa29d441c662d982fbe68bf3e394291
Minddol <minddol@gmail.com>
2021-03-05 16:07:35 +0900

    Call `VoiceChromePresenterDelegate.voiceChromeChipsDidClick(chips:)` when chips button clicked. (#768)

--- NuguClientKit/Sources/Presenter/VoiceChromePresenter.swift
+++ NuguClientKit/Sources/Presenter/VoiceChromePresenter.swift
@@ -165,7 +165,9 @@ public extension VoiceChromePresenter {
         log.debug("")
         
         if let chipsData = chipsData {
-            nuguVoiceChrome.setChipsData(chipsData: chipsData)
+            nuguVoiceChrome.setChipsData(chipsData) { [weak self] chips in
+                self?.delegate?.voiceChromeChipsDidClick(chips: chips)
+            }
         }
         voiceChromeDismissWorkItem?.cancel()
         nuguVoiceChrome.removeFromSuperview()
@@ -218,12 +220,14 @@ private extension VoiceChromePresenter {
     }
     
     func setChipsButton(actionList: [(text: String, token: String?)], normalList: [(text: String, token: String?)]) {
-        var chipsButtonList = [NuguChipsButton.NuguChipsButtonType]()
+        var chipsData = [NuguChipsButton.NuguChipsButtonType]()
         let actionButtonList = actionList.map { NuguChipsButton.NuguChipsButtonType.action(text: $0.text, token: $0.token) }
-        chipsButtonList.append(contentsOf: actionButtonList)
+        chipsData.append(contentsOf: actionButtonList)
         let normalButtonList = normalList.map { NuguChipsButton.NuguChipsButtonType.normal(text: $0.text, token: $0.token) }
-        chipsButtonList.append(contentsOf: normalButtonList)
-        nuguVoiceChrome.setChipsData(chipsData: chipsButtonList)
+        chipsData.append(contentsOf: normalButtonList)
+        nuguVoiceChrome.setChipsData(chipsData) { [weak self] chips in
+            self?.delegate?.voiceChromeChipsDidClick(chips: chips)
+        }
     }
     
     func disableIdleTimer() {
