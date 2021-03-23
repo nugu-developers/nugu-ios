5b2a7ac71b6022f09860fef781de32ee4e01f114
김진님/AI Assistant개발 Cell <jin.marino@sktelecom.com>
2021-03-23 17:52:33 +0900

    Remove guideTextLabel's text during session has been activated

--- NuguClientKit/Sources/Presenter/VoiceChromePresenter.swift
+++ NuguClientKit/Sources/Presenter/VoiceChromePresenter.swift
@@ -172,6 +172,9 @@ public extension VoiceChromePresenter {
         voiceChromeDismissWorkItem?.cancel()
         nuguVoiceChrome.removeFromSuperview()
         nuguVoiceChrome.changeState(state: .listeningPassive)
+        if nuguClient?.dialogStateAggregator.sessionActivated == true {
+            nuguVoiceChrome.setRecognizedText(text: nil)
+        }
         
         try showVoiceChrome()
     }
@@ -338,7 +341,9 @@ private extension VoiceChromePresenter {
                     // If voice chrome is not showing or dismissing in listening state, voice chrome should be presented
                     try? self.showVoiceChrome()
                     if notification.multiTurn || notification.sessionActivated {
-                        self.nuguVoiceChrome.changeState(state: .listeningPassive)
+                        if self.nuguVoiceChrome.currentState != .listeningPassive {
+                            self.nuguVoiceChrome.changeState(state: .listeningPassive)
+                        }
                         self.nuguVoiceChrome.setRecognizedText(text: nil)
                     }
                     if let chips = notification.chips {
