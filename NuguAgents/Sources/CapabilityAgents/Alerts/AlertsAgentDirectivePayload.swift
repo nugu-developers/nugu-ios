4efa4d9a016330114ae603270ecb1d6eb487479f
Yonghoon Kwon <yhoonyk@gmail.com>
2021-05-03 10:33:02 +0900

    Add initializer in `AlertsAgentDirectivePayload` (#826)

--- NuguAgents/Sources/CapabilityAgents/Alerts/AlertsAgentDirectivePayload.swift
+++ NuguAgents/Sources/CapabilityAgents/Alerts/AlertsAgentDirectivePayload.swift
@@ -44,6 +44,42 @@ public struct AlertsAgentDirectivePayload {
         public let assetRequiredInMilliseconds: Int?
         /// <#Description#>
         public let assets: [AlertsAsset]
+        
+        /// <#Description#>
+        /// - Parameters:
+        ///   - playServiceId: <#playServiceId description#>
+        ///   - token: <#token description#>
+        ///   - alertType: <#alertType description#>
+        ///   - scheduledTime: <#scheduledTime description#>
+        ///   - activation: <#activation description#>
+        ///   - minDurationInSec: <#minDurationInSec description#>
+        ///   - repeat: <#repeat description#>
+        ///   - alarmResourceType: <#alarmResourceType description#>
+        ///   - assetRequiredInMilliseconds: <#assetRequiredInMilliseconds description#>
+        ///   - assets: <#assets description#>
+        public init(
+            playServiceId: String,
+            token: String,
+            alertType: String,
+            scheduledTime: String,
+            activation: Bool,
+            minDurationInSec: Int?,
+            `repeat`: AlertsRepeat?,
+            alarmResourceType: String?,
+            assetRequiredInMilliseconds: Int?,
+            assets: [AlertsAsset]
+        ) {
+            self.playServiceId = playServiceId
+            self.token = token
+            self.alertType = alertType
+            self.scheduledTime = scheduledTime
+            self.activation = activation
+            self.minDurationInSec = minDurationInSec
+            self.repeat = `repeat`
+            self.alarmResourceType = alarmResourceType
+            self.assetRequiredInMilliseconds = assetRequiredInMilliseconds
+            self.assets = assets
+        }
     }
     
     /// <#Description#>
@@ -52,6 +88,15 @@ public struct AlertsAgentDirectivePayload {
         public let playServiceId: String
         /// <#Description#>
         public let tokens: [String]
+        
+        /// <#Description#>
+        /// - Parameters:
+        ///   - playServiceId: <#playServiceId description#>
+        ///   - tokens: <#tokens description#>
+        public init(playServiceId: String, tokens: [String]) {
+            self.playServiceId = playServiceId
+            self.tokens = tokens
+        }
     }
     
     /// <#Description#>
