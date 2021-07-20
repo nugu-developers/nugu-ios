2f166ade5922f7df99ab9a9f2199f1dc9340f357
childc <skimdcc@gmail.com>
2021-07-21 01:41:16 +0900

    Remove configuration file

--- SampleApp/Sources/AppDelegate.swift
+++ SampleApp/Sources/AppDelegate.swift
@@ -29,7 +29,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        ConfigurationStore.shared.configure(url: Bundle.main.url(forResource: "nugu_config-tPhone", withExtension: "plist")!)
+        ConfigurationStore.shared.configure()
         return true
     }
 
