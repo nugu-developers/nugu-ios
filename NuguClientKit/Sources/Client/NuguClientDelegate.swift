fe13b18ae44811816d74090c68a488da8021dff2
김진님/AI Assistant개발 Cell <jin.marino@sktelecom.com>
2021-07-28 15:12:05 +0900

    Add comment

--- NuguClientKit/Sources/Client/NuguClientDelegate.swift
+++ NuguClientKit/Sources/Client/NuguClientDelegate.swift
@@ -25,7 +25,8 @@ import NuguCore
 /// <#Description#>
 public protocol NuguClientDelegate: AnyObject {
     // audio session related
-    /// Notify that  nugu client will play some audio
+    /// Notify that  nugu client should update audio session
+    /// - Parameter requestingFocus: whether updating AudioSession is for requesting focus or just updating without requesting focus
     @discardableResult func nuguClientShouldUpdateAudioSession(requestingFocus: Bool) -> Bool
     
     /// Notify that  nugu client won't play sound anymore.
