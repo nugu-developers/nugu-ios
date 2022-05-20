ee21caf1712aeb93f69bab49f52d6d9402467ccc
yHoonyK <yhoonyk@gmail.com>
2022-05-20 13:53:03 +0900

    Adds `MessengerAgent`

---
+++ NuguAgents/Sources/CapabilityAgents/Messenger/MessengerProfile.swift
@@ -0,0 +1,45 @@
+//
+//  MessengerProfile.swift
+//  NuguMessengerAgent
+//
+//  Created by yonghoonKwon on 2021/04/13.
+//  Copyright (c) 2021 SK Telecom Co., Ltd. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import Foundation
+
+public struct MessengerProfile: Codable {
+    /// <#Description#>
+    public let key: String
+    /// <#Description#>
+    public let name: String
+    /// <#Description#>
+    public let imageUrl: String
+    
+    /// <#Description#>
+    /// - Parameters:
+    ///   - key: <#key description#>
+    ///   - name: <#name description#>
+    ///   - imageUrl: <#imageUrl description#>
+    public init(
+        key: String,
+        name: String,
+        imageUrl: String
+    ) {
+        self.key = key
+        self.name = name
+        self.imageUrl = imageUrl
+    }
+}
