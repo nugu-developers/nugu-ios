71d3ec8d6fc93e10788d494aab1ef5ad16ac893d
jayce1116 <jaycesub@gmail.com>
2022-08-22 09:04:13 +0900

    attach license

--- NuguTests/Mock/MockUpstreamDataSender.swift
+++ NuguTests/Mock/MockUpstreamDataSender.swift
@@ -2,8 +2,20 @@
 //  MockUpstreamDataSender.swift
 //  NuguTests
 //
-//  Created by 신정섭님/A.출시 on 2022/08/18.
+//  Created by jaycesub on 2022/08/18.
 //  Copyright © 2022 SK Telecom Co., Ltd. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 //
 
 import Foundation
