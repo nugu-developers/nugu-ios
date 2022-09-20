8bbc9201cf165c454d762fb43a79ae345459fbbe
childc <skimdcc@gmail.com>
2022-09-20 14:48:35 +0900

    Make `InterfactionControl` payload not to `Data` but `Dictionary` (#1002)
    (cherry picked from commit 200304decfab97114c723867377cfee48097f6cd)

--- NuguAgents/Sources/CapabilityAgents/PhoneCall/PhoneCallAgent+Event.swift
+++ NuguAgents/Sources/CapabilityAgents/PhoneCall/PhoneCallAgent+Event.swift
@@ -47,8 +47,9 @@ extension PhoneCallAgent.Event: Eventable {
         switch typeInfo {
         case .candidatesListed(let interactionControl):
             if let interactionControl = interactionControl,
-               let interactionControlPayload = try? JSONEncoder().encode(interactionControl) {
-                payload["interactionControl"] = interactionControlPayload
+               let interactionControlData = try? JSONEncoder().encode(interactionControl),
+               let interactionControlDictionary = try? JSONSerialization.jsonObject(with: interactionControlData, options: []) as? [String: AnyHashable] {
+                payload["interactionControl"] = interactionControlDictionary
             }
         case .makeCallFailed(let errorCode, let callType):
             payload["errorCode"] = errorCode.rawValue
