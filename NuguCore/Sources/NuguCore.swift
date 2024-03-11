65c8f0a4ce2fa7a4e822aa2efd50d0e0960b26b6
jayce1116 <jayceSub@gmail.com>
2024-03-12 07:23:03 +0900

    update version to `1.9.3` (#1139)
    ### Description
    - update version to `1.9.3`

--- NuguCore/Sources/NuguCore.swift
+++ NuguCore/Sources/NuguCore.swift
@@ -56,5 +56,5 @@ public var logLevel: NattyLog.LogLevel {
 public let nuguSDKVersion = (Bundle(for: AuthorizationStore.self).object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? "0")
 #else
 // FIXME: 현재는 SPM에서 버전을 가져올 방법이 없다.
-public let nuguSDKVersion = "1.9.2"
+public let nuguSDKVersion = "1.9.3"
 #endif
